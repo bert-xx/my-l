f7abd55aef0632a2a5d11fa53b71159419b0a337
bert-xx <nowertx@gmail.com>
2026-04-23 18:39:12 +0300

    x3.js

--- x3.js
+++ x3.js
@@ -4,11 +4,11 @@
     Lampa.Listener.follow('app', (e) => {
         if (e.type == 'ready') {
             setTimeout(function () {
-                // Удаляем Аниме
+               
                 $("[data-action=anime]").remove();
-                // Удаляем Индийское
+           
                 $("[data-action=indian]").remove();
-            }, 100); // Немного увеличил задержку для надежности
+            }, 100); 
         }
     });
 
