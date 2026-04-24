7f85af8a56203617ecba62faf9d94ebbb7dc7221
bert-xx <nowertx@gmail.com>
2026-04-24 09:51:48 +0300

    x3.js

--- x3.js
+++ x3.js
@@ -1,14 +1,27 @@
 (function () {
     'use strict';	
 
+    
+    var style = $('<style>' +
+        '[data-action="anime"], ' +   // Скрывает Аниме
+        '[data-action="indian"]' +    // Скрывает Индийское
+        '{ display: none !important; }' +
+    '</style>');
+    
+    $('head').append(style);
+
+    
     Lampa.Listener.follow('app', (e) => {
         if (e.type == 'ready') {
-            setTimeout(function () {
-               
-                $("[data-action=anime]").remove();
-           
-                $("[data-action=indian]").remove();
-            }, 100); 
+            var clear = function() {
+                $("[data-action='anime']").remove();
+                $("[data-action='indian']").remove();
+            };
+            
+            
+            setTimeout(clear, 10); 
+            setTimeout(clear, 500);
+            setTimeout(clear, 2000);
         }
     });
 
